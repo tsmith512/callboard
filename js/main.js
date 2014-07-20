7eb3041e435bc33fc9b275e1233d5c5944312086
Taylor Smith <tsmith@tsmithcreative.com>
2014-07-20 21:16:07 +0000

    Cleanups, efficiencies, code comments

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@
       eventContainer = '<li class="event-unprocessed"></li>',
       countdownContainer = '<span class="countdown"></span>';
 
+  // Run through the call list to build out the necessary warning events:
   $.each(calls, function(index, call){
     call.mtime = moment(call.time, 'hh:mma');
     call.time = call.mtime.toDate();
 
+    // For each warning in this call ("X minutes until EVENT"), make a new warning event
     $.each(call.warnings, function(index, minutes){
       warnings.push({
         mtime: moment(call.mtime).subtract('minutes', minutes),
@@ -21,9 +23,11 @@
     })
   });
 
+  // Sort both the calls and warnings arrays by time
   calls.sort(function(a,b) { return (a.time) - (b.time) } );
   warnings.sort(function(a,b) { return (a.time) - (b.time) } );
 
+  // For each warning event, build out the message and put it in the call list
   $.each(warnings, function(index, warning){
     var count = countdown(null, warning.mtime.toDate()),
         time = warning.mtime.format('hh:mma')
@@ -32,44 +36,69 @@
           callMessage.replace('COUNT', countdownContainer).replace('EVENT', warning.event).replace('TIME', time);
 
     $(eventContainer)
-      .attr('id', 'warning-' + index)
       .data('mtime', warning.mtime.toDate())
       .data('type', 'warning')
       .html(content)
-      .appendTo('ul#A');
+      .appendTo('#calls');
   });
 
+  // For each call event, build out the message and add it to the schedule list
   $.each(calls, function(index, call){
     var count = countdown(null, call.mtime.toDate());
     $(eventContainer)
-      .attr('id', 'call-' + index)
       .data('mtime', call.mtime.toDate())
       .data('type', 'call')
       .html(scheduleMessage.replace('TIME', call.mtime.format('hh:mma')).replace('COUNT', countdownContainer).replace('EVENT', call.event))
-      .appendTo('ul#B');
+      .appendTo('#schedule');
   });
 
+  // Add to the Number prototype a few functions to make testing more legible:
+    // A function to see if one number is between two others
+    Number.prototype.between = function(low,high){ return (this > low && this <= high); }
+
+    // A function to see if a time is in the future (is it positive?)
+    Number.prototype.future =  function(){ return (this > 0); }
+
+  // A function to update each countdown label and add/remove classes
   var updateEvents = function() {
+    var now = false;
+
     $('li').each(function(){
       var count = countdown(null, $(this).data('mtime')),
           minutesRemaining = countdown(null, $(this).data('mtime'), countdown.MINUTES),
-          remaining = (count.value > 0) ? [count.hours, count.minutes, count.seconds].join(":") : (minutesRemaining.minutes + ' minutes ago');
+          remaining = (count.value > 0) ? [count.hours, count.minutes, count.seconds].join(":") : (minutesRemaining.minutes + ' minutes ago'),
+          newClass = '';
+
+      // Do we have a call at this exact moment?
+      if (minutesRemaining.minutes == 0) { now = true; }
 
       // Add classes to the list item based on how long
-      if (count.value > 0 && minutesRemaining.minutes > 15) { $(this).removeClass().addClass('event-future'); }
-      if (count.value > 0 && minutesRemaining.minutes > 5 && minutesRemaining.minutes <= 15) { $(this).removeClass().addClass('event-upcoming'); }
-      if (count.value > 0 && minutesRemaining.minutes > 2 && minutesRemaining.minutes <= 5) { $(this).removeClass().addClass('event-soon'); }
-      if (count.value > 0 && minutesRemaining.minutes <= 2) { $(this).removeClass().addClass('event-call'); }
-      else if (count.value < 0 && minutesRemaining.minutes <= 5) { $(this).removeClass().addClass('event-recent'); }
-      else if (count.value < 0 && minutesRemaining.minutes > 5) {
-        $(this).removeClass().addClass('event-past');
-        if ( $(this).data('type') == 'warning' ) { $(this).remove(); }
+      if (count.value.future()) {
+        // Future calls:
+        if      (minutesRemaining.minutes > 15)            { newClass = 'event-future';   }
+        else if (minutesRemaining.minutes.between(5, 15))  { newClass = 'event-upcoming'; }
+        else if (minutesRemaining.minutes.between(2, 5))   { newClass = 'event-soon';     }
+        else if (minutesRemaining.minutes.between(-1, 2))  { newClass = 'event-call';     }
+      } else {
+        // Now / Past calls:
+        if      (minutesRemaining.minutes.between(-1, 1))  { newClass = 'event-call';     }
+        else if (minutesRemaining.minutes.between(1, 5))   { newClass = 'event-recent';   }
+        else if (minutesRemaining.minutes > 5)             { newClass = 'event-past';     }
       }
+
+      $(this).removeClass().addClass(newClass);
+
+      // Warnings can be removed from the DOM after they're expired
+      if ( $(this).data('type') == 'warning' && newClass == 'event-past' ) { $(this).remove(); }
 
       // Update the readout of the countdown:
       $('.countdown', this).text(remaining);
     });
+
+    // For thirty seconds before/after an exact call time, add a body class.
+    if (now) { $('body').addClass('call-now');    }
+    else     { $('body').removeClass('call-now'); }
   }
 
-  var refresh = window.setInterval(updateEvents, 500);
+  var refresh = window.setInterval(updateEvents, 1000);
 })(jQuery);
