33573f2582a02b475911997173de63638be51c2b
Taylor Smith <tsmith@tsmithcreative.com>
2014-07-20 15:16:30 +0000

    First working prototype: running countdowns for all calls and each warning value, sorted

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
+var warnings = [];
+
 ;(function($){
+
   $.each(calls, function(index, call){
     call.mtime = moment(call.time, 'hh:mma');
-    call.count = countdown(new Date(), call.mtime.toDate());
-    var remaining = [call.count.hours, call.count.minutes, call.count.seconds].join(":")
-    $('<li/>').attr('id', 'call-' + index).text([call.event, remaining].join(' ')).appendTo('ul');
+    call.time = call.mtime.toDate();
+
+    $.each(call.warnings, function(warningindex, minutes){
+      warnings.push({
+        mtime: moment(call.mtime).subtract('minutes', minutes),
+        time: moment(call.mtime).subtract('minutes', minutes).toDate(),
+        minute: minutes,
+        forEvent: index,
+      });
+    })
+  });
+
+  calls.sort(function(a,b) { return (a.time) - (b.time) } );
+  warnings.sort(function(a,b) { return (a.time) - (b.time) } );
+
+  $.each(warnings, function(index, warning){
+    var count = countdown(null, warning.mtime.toDate());
+    var remaining = '(' + [count.hours, count.minutes, count.seconds].join(":") + ')';
+    $('<li/>').attr('id', 'warning-' + index).text([warning.mtime.format('hh:mma'), '-', warning.minute, 'minutes to', calls[warning.forEvent].event, remaining].join(' ')).appendTo('ul#A');
+  });
+
+  $.each(calls, function(index, call){
+    var count = countdown(null, call.mtime.toDate());
+    var remaining = [count.hours, count.minutes, count.seconds].join(":")
+    $('<li/>').attr('id', 'call-' + index).text([call.event, remaining].join(' ')).appendTo('ul#B');
   });
 
   var updateCalls = function() {
     $.each(calls, function(index, call){
-      var remaining = [call.count.hours, call.count.minutes, call.count.seconds].join(":")
+      var count = countdown(null, call.mtime.toDate());
+      var remaining = [count.hours, count.minutes, count.seconds].join(":")
       $('li#call-' + index).text([call.event, remaining].join(' '));
+    });
+    $.each(warnings, function(index, warning){
+      var count = countdown(null, warning.mtime.toDate());
+      var remaining = '(' + [count.hours, count.minutes, count.seconds].join(":") + ')';
+      $('li#warning-' + index).text([warning.mtime.format('hh:mma'), '-', warning.minute, 'minutes to', calls[warning.forEvent].event, remaining].join(' '));
     });
   }
 
