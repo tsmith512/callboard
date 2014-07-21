87c19e7cab483d305e4ca7d2bf58b6662ddedac8
Taylor Smith <tsmith@tsmithcreative.com>
2014-07-21 23:00:33 +0000

    Sample data file which schedules placeholder calls relative to the page load time, for a more exciting demo!

--- js/calls.js
+++ js/calls.js
@@ -1,37 +1,36 @@
+// This is a sample version of calls which sets up calls and warnings relative
+// to when the page was loaded, for demonstration purposes. For each of these,
+// 'time' should be a value like '7:30pm' instead.
+
 var calls = [
   {
+    event: 'Page loaded',
+    time: moment().format('hh:mma'),
+    warnings: [] // Warnings can be empty, if the event just needs to be on the schedule
+  },
+  {
     event: 'Actors to Venue',
-    time: '7:00pm',
+    time: moment().subtract('hour', 1).format('hh:mma'),
     warnings: [0]
   },
   {
-    event: 'Fight and Fall Rehearsals',
-    time: '7:15pm',
-    warnings: [10, 0]
+    event: 'Fight Rehearsals',
+    time: moment().add('minutes', 7).format('hh:mma'),
+    warnings: [5, 0]
   },
   {
     event: 'Sound Check',
-    time: '8:00pm',
+    time: moment().add('minutes', 20).format('hh:mma'),
     warnings: [10, 0]
   },
   {
-    event: 'Openers to Places',
-    time: '8:23pm',
-    warnings: [15, 5, 0]
-  },
-  {
     event: 'Actors and Band to Places',
-    time: '8:28pm',
+    time: moment().add('minutes', 63).format('hh:mma'),
     warnings: [60, 30, 15, 10, 5, 0]
   },
   {
-    event: 'Call the Hill',
-    time: '8:28pm',
-    warnings: [0]
-  },
-  {
     event: 'Standby Sound; Go Band',
-    time: '8:30pm',
+    time: moment().add('minutes', 65).format('hh:mma'),
     warnings: [0]
   }
 ];
