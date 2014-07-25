8100b39f546670690ade4f12027257c6559a4720
Taylor Smith <tsmith@tsmithcreative.com>
2014-07-25 14:32:57 +0000

    Timeline updates

--- js/calls.js
+++ js/calls.js
@@ -6,23 +6,28 @@ var calls = [
   },
   {
     event: 'Fight and Fall Rehearsals',
-    time: '7:10pm',
+    time: '7:15pm',
     warnings: [5, 0]
   },
   {
     event: 'Sound Check',
     time: '8:00pm',
-    warnings: [10, 0]
+    warnings: []
+  },
+  {
+    event: 'Top of Show',
+    time: '8:30pm',
+    warnings: [30, 15, 10]
   },
   {
     event: 'Openers to Places',
     time: '8:23pm',
-    warnings: [15, 5, 0]
+    warnings: [0]
   },
   {
-    event: 'Places',
-    time: '8:28pm',
-    warnings: [60, 30, 15, 10, 5, 0]
+    event: 'Go Craig',
+    time: '8:25pm',
+    warnings: [0]
   },
   {
     event: 'Call the Hill',
