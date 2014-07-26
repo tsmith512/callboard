9c019cfa4e717b5f4921e7afe33db92fcf85e5d7
Taylor Smith <tsmith@tsmithcreative.com>
2014-07-26 15:42:10 +0000

    Oklahoma timeline updates per 7/25

--- js/calls.js
+++ js/calls.js
@@ -5,14 +5,14 @@ var calls = [
     warnings: [0]
   },
   {
-    event: 'Many a New Day Dance Call',
-    time: '7:00pm',
-    warnings: [5, 0]
-  },
-  {
     event: 'Fight and Fall Rehearsals',
     time: '7:10pm',
     warnings: [5, 0]
+  },
+  {
+    event: 'Will & Gertie Meet and Greet (Saturdays)',
+    time: '7:30pm',
+    warnings: [10, 0]
   },
   {
     event: 'Sound Check',
@@ -21,13 +21,13 @@ var calls = [
   },
   {
     event: 'Top of Show',
-    time: '8:30pm',
-    warnings: [30, 15, 10]
+    time: '8:25pm',
+    warnings: [30, 15, 10, 5]
   },
   {
-    event: 'Openers to Places',
-    time: '8:23pm',
-    warnings: [0]
+    event: 'Places',
+    time: '8:25pm',
+    warnings: [2]
   },
   {
     event: 'Go Craig',
